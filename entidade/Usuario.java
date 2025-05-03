4883a00dfc0d3cb5a03815c9f97732c1d09a0639
victorgabdev <victorgabdev@gmail.com>
2025-05-03 15:59:49 -0300

    capitulo 7.3

--- entidade/Usuario.java
+++ entidade/Usuario.java
@@ -31,4 +31,9 @@ public class Usuario {
     public boolean isModerador() {
         return moderador;
     }
+
+    @Override
+    public String toString() {
+        return nome + "/" + pontos;
+    }
 }
---
+++ main/Capitulo7.java
@@ -0,0 +1,56 @@
+package main;
+
+import entidade.Usuario;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Capitulo7 {
+    public static void main(String[] args) {
+        Usuario user1 = new Usuario("Paulo Silveira", 150);
+        Usuario user2 = new Usuario("Rodrigo Turini", 120);
+        Usuario user3 = new Usuario("Guilherme Silveira", 190);
+        Usuario user4 = new Usuario("Juliana Castro", 175);
+        Usuario user5 = new Usuario("Rafael Oliveira", 130);
+        Usuario user6 = new Usuario("Camila Fernandes", 160);
+        Usuario user7 = new Usuario("Bruno Costa", 50);
+        Usuario user8 = new Usuario("Mariana Souza", 200);
+        Usuario user9 = new Usuario("Diego Almeida", 140);
+        Usuario user10 = new Usuario("Paula Mendes", 30);
+
+        List<Usuario> usuarios = new ArrayList<>();
+
+        usuarios.add(user1);
+        usuarios.add(user2);
+        usuarios.add(user3);
+        usuarios.add(user4);
+        usuarios.add(user5);
+        usuarios.add(user6);
+        usuarios.add(user7);
+        usuarios.add(user8);
+        usuarios.add(user9);
+        usuarios.add(user10);
+
+        // filtrar os 10 usuarios com mais pontos e torná-los moderadores
+        //usuarios.sort(Comparator.comparingInt(Usuario::getPontos).reversed());  // maior para o menor
+        //usuarios.forEach(System.out::println);
+
+        // filtrar todos os usuários que têm mais de 100 pontos
+        //Maneira Antiga
+        for (Usuario usuario : usuarios) {
+            if(usuario.getPontos() > 100) usuario.tornaModerador();
+        }
+
+        usuarios.stream()
+                .filter(usuario -> usuario.getPontos() > 100)  // filtro todos os usuarios que tem mais de 100 pontos
+                .forEach(Usuario::tornaModerador);  // transformo em moderadores
+
+
+
+
+
+    }
+}
